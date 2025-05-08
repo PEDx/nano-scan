de9e8dbd1464e84b1f3b50a28b71881b8e10df18
alan.peng <pengdong5566@163.com>
2025-05-08 11:52:03 +0800

    refactor: restructure rolldown configuration to support multiple output formats and add new nano-scan.js file

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'rolldown';
 import typescript from '@rollup/plugin-typescript';
 
-export default defineConfig({
-  input: 'lib/index.ts',
-  output: [
-    {
-      file: 'dist/umd/index.js',
-      format: 'umd',
-      name: 'NanoScan',
-    },
-    {
+export default defineConfig([
+  {
+    input: 'lib/index.ts',
+    output: [
+      {
+        file: 'dist/umd/index.js',
+        format: 'umd',
+        name: 'NanoScan',
+      },
+      {
+        file: 'dist/esm/index.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.json' })],
+  },
+  {
+    input: 'lib/index.ts',
+    output: {
       file: 'docs/nano-scan.js',
       format: 'umd',
       name: 'NanoScan',
     },
-    {
-      file: 'dist/esm/index.js',
-      format: 'esm',
-    },
-  ],
-  plugins: [typescript({ tsconfig: './tsconfig.json' })],
-});
+  },
+]);
